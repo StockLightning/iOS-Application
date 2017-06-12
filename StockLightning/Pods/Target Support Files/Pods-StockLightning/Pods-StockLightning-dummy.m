87040d8843c5ad64a3a4855f990de19b2df979eb
asbasi <arvinder.singh.basi@gmail.com>
2017-06-11 20:21:54 -0700

    Added podfile and basic layout of app.

---
+++ StockLightning/Pods/Target Support Files/Pods-StockLightning/Pods-StockLightning-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_StockLightning : NSObject
+@end
+@implementation PodsDummy_Pods_StockLightning
+@end
---
+++ StockLightning/StockLightning/MainTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MainTableViewCell.swift
+//  StockLightning
+//
+//  Created by Arvinder Basi on 6/11/17.
+//  Copyright © 2017 Arvinder Basi. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
